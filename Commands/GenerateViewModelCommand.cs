b0918027ed3301658571e37576e2fbe9724bb0e0
lijianing <lijianing@wisekingrobot.com>
2025-02-17 16:58:19 +0800

    添加项目文件。

---
+++ Commands/GenerateViewModelCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace MvvmHelper.Commands
+{
+    [Command(PackageIds.GenerateViewModelCommand)]
+    internal sealed class GenerateViewModelCommand : BaseCommand<GenerateViewModelCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            SolutionItem activeItem = await VS.Solutions.GetActiveItemAsync();
+            string fullPath = string.Empty;
+            string targetFileName = string.Empty;
+            string targetPath = string.Empty;
+            string filePath = activeItem?.FullPath;
+            string fileName = string.Empty;
+            if (filePath != null)
+            {
+                fullPath = Path.GetDirectoryName(filePath);
+            }
+
+            if (fullPath != null)
+            {
+                targetPath = fullPath.Replace("Views", "ViewModels");
+            }
+
+            if (activeItem != null)
+            {
+                fileName = Path.GetFileNameWithoutExtension(filePath);
+                targetFileName =fileName + "ViewModel.cs";
+            }
+
+            CreateViewModelFileAsync(targetPath, targetFileName,fileName);
+            Project project = await VS.Solutions.GetActiveProjectAsync();
+
+            if (project != null) await project.AddExistingFilesAsync(new[]{targetPath+targetFileName});
+
+        }
+
+        private static void CreateViewModelFileAsync(string filePath, string fileName,string viewName)
+        {
+            if (!File.Exists(filePath+fileName))
+            {
+                File.Create(filePath + fileName);
+            }
+
+        }
+    }
+}
---
+++ MvvmHelperPackage.cs
@@ -0,0 +1,29 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace MvvmHelper
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.MvvmHelperString)]
+    [ProvideUIContextRule("660bc25f-3a95-48e6-ac0d-a8b8eda0fbce", "XAML Files Only", "XAML",
+        ["Xaml"],
+        ["HierSingleSelectionName:.xaml$"])]
+    // [ProvideUIContextRule("660bc25f-3a95-48e6-ac0d-a8b8eda0fbce",
+    //    "Supported Files",
+    //     "CSharp | VisualBasic",
+    //     ["CSharp", "VisualBasic"],
+    //     ["HierSingleSelectionName:.cs$", "HierSingleSelectionName:.vb$"])]
+    public sealed class MvvmHelperPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
